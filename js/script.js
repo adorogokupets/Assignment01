7839cd0b59500c1289973458ab982a5612864912
Alexey Dorogokupets <adorogokupets@gmail.com>
2024-02-18 01:11:16 -0800

    M2 Assignment

---
+++ js/script.js
@@ -0,0 +1,138 @@
+// // STEP 1
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// // STEP 2
+// let num = 10
+// let phrase = 'Never give up'
+// let isItTrue = true
+// let nothing = null
+
+// // STEP 3
+// let num1 = 5
+// let num2 = 3
+
+// let result = num1 * num2
+// console.log(result)
+
+// let word1 = 'This is '
+// let word2 = 'STEP 3'
+
+// let phrase = word1 + word2
+// console.log(phrase)
+
+// // STEP 4
+// let strFirstName, strLastName, strAddress, strCity, strReferralSource
+// let intZipCode, intYourAge
+// let blnMayWeContactYou
+
+// // STEP 5
+// var strFirstName
+// var intYourAge
+// var blnMayWeContactYou
+
+// strFirstName = 'John'
+// intYourAge = 35
+// blnMayWeContactYou = true
+
+// var strFirstName = 'Bob'
+// var intYourAge = 29
+// var blnMayWeContactYou = false
+
+// var strFirstName = 'Anna', intYourAge = 36, blnMayWeContactYou = true
+
+// // STEP 6
+// let numStr = 'Number is ' + 6
+// console.log(numStr)
+
+// // STEP 7
+// let firstVariable = true + ' display'
+// let secondVariable = 15 + false
+// console.log(firstVariable)
+// console.log(secondVariable)
+
+// // STEP 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// // STEP 9
+// let nothing = null
+// console.log(nothing)
+
+// let mystery
+// console.log(mystery)
+
+// // STEP 10
+// console.log(typeof 'String')
+// console.log(typeof 1)
+// console.log(typeof true)
+// console.log(typeof {})
+// console.log(typeof undefined)
+
+// // STEP 11
+// alert('Hello ' + 'Alex Dorogokupets' + ', welcome to the JavaScript class!')
+
+// // STEP 12
+// let fullName = 'Alex Dorogokupets'
+// alert('Hello ' + fullName + ', welcome to the JavaScript class!')
+
+// // STEP 13
+// let course = 'JavaScript'
+// alert(`Hello ${fullName}, welcome to the ${course} class!`)
+
+// // STEP 14
+// alert(`Hello ${fullName}.\nWelcome to the ${course} class!`)
+
+// // STEP 15
+// alert(`Hello ${prompt('Enter your name')}.\nWelcome to the ${course} class!`)
+
+// // STEP 16
+// alert(`Hello ${prompt('Enter your name')}.\nWelcome to the ${prompt('Enter name of the class you are taking')} class!`)
+
+// // STEP 17
+// let x = 10
+// let y = 20
+
+// console.log(x+y)
+
+// // STEP 18
+// let x = 20
+// x += 20
+
+// console.log(x)
+
+// // STEP 19
+// let x = 20
+// x *= 5
+
+// console.log(x)
+
+// // STEP 20
+// let x = 20 % 3
+// x /= 1
+
+// console.log(x)
+
+// // STEP 21
+// let a = 10
+// let b = 5
+// let c = 5
+
+// let result = (a < b) || (a - b === c)
+
+// console.log(result)
+
+// STEP 22
+
+let x = 10
+let y = 5
+let z = 5
+
+let result = (x < y) && (x - y === z)
+console.log(result)
+
+
+
